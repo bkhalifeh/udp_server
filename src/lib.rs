aacae339fe8e3396da68f81da4e033590beba3db
Behzad Khalifeh <bkhalifeh@protonmail.com>
2025-04-16 01:03:23 +0330

    chore: remove unused function setup_socket

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,6 @@
 use std::env;
 use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 use std::str::FromStr;
-use tokio_uring::net::UdpSocket;
-
-pub async fn setup_socket() -> Result<UdpSocket, Box<dyn std::error::Error>> {
-    let host: String = env::var("APP_HOST")?;
-    let port: u16 = env::var("APP_PORT")?.parse()?;
-    let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::from_str(&host)?), port);
-
-    Ok(UdpSocket::bind(addr.clone()).await?)
-}
 
 pub fn get_server_addr() -> Result<SocketAddr, Box<dyn std::error::Error>> {
     let host: String = env::var("APP_HOST")?;
